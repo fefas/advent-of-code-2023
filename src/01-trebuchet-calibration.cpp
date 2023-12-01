886dddaaf0093a49abe6a2281a8c6ece7d1e26aa
Felipe Martins <6557756+fefas@users.noreply.github.com>
2023-12-01 18:55:59 +0100

    Refactor

--- src/01-trebuchet-calibration.cpp
+++ src/01-trebuchet-calibration.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 #include "01-trebuchet-calibration.h"
 
@@ -7,28 +8,27 @@ using namespace std;
 
 int Solution_01::sumCalibrations(string input)
 {
+    vector<string> spelledNumbers = {
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+    };
+
     long ans = 0;
     short lastDig = -1;
 
     for (int i = 0; i < input.size(); i++) {
-        if (input.substr(i, 3) ==  "one") {
-            input[i] = '1';
-        } else if (input.substr(i, 3) ==  "two") {
-            input[i] = '2';
-        } else if (input.substr(i, 5) ==  "three") {
-            input[i] = '3';
-        } else if (input.substr(i, 4) ==  "four") {
-            input[i] = '4';
-        } else if (input.substr(i, 4) ==  "five") {
-            input[i] = '5';
-        } else if (input.substr(i, 3) ==  "six") {
-            input[i] = '6';
-        } else if (input.substr(i, 5) ==  "seven") {
-            input[i] = '7';
-        } else if (input.substr(i, 5) ==  "eight") {
-            input[i] = '8';
-        } else if (input.substr(i, 4) ==  "nine") {
-            input[i] = '9';
+
+        for (int j = 0; j < 9; j++) {
+            if (input.substr(i, spelledNumbers[j].size()) ==  spelledNumbers[j]) {
+                input[i] = (char) (j + 1 + '0');
+            }
         }
 
         char c = input[i];
