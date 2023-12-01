02bd07eadcdf5c6c787b255e2da53e584d1414e2
Felipe Martins <6557756+fefas@users.noreply.github.com>
2023-12-02 00:47:16 +0100

    Refactor 2

--- src/01-trebuchet-calibration.cpp
+++ src/01-trebuchet-calibration.cpp
@@ -6,42 +6,65 @@
 
 using namespace std;
 
-int Solution_01::sumCalibrations(string input)
+class CalibrationsWalker
 {
-    vector<string> spelledNumbers = {
-        "one",
-        "two",
-        "three",
-        "four",
-        "five",
-        "six",
-        "seven",
-        "eight",
-        "nine",
-    };
+    private:
+    string messedCalibrations;
+    vector<string> spelledNumbers = { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+    int i;
 
-    long ans = 0;
-    short lastDig = -1;
+    public:
+    static const short LINE_BREAK = -1;
+    static const short NOT_A_NUMBER = -2;
+    static const short END_OF_STRING = -3;
 
-    for (int i = 0; i < input.size(); i++) {
-
-        for (int j = 0; j < 9; j++) {
-            if (input.substr(i, spelledNumbers[j].size()) ==  spelledNumbers[j]) {
-                input[i] = (char) (j + 1 + '0');
-            }
-        }
-
-        char c = input[i];
-        if (c == '\n') {
-            ans += lastDig;
-            lastDig = -1;
-        } else if (c >= '0' && c <= '9') {
-            int val = c - '0';
-            if (lastDig == -1) ans += 10 * val;
-            lastDig = val;
-        }
+    CalibrationsWalker(string input)
+    {
+        messedCalibrations = input;
+        i = 0;
     }
 
-    return ans + lastDig;
+    short getNext()
+    {
+        if (i == messedCalibrations.size()) return END_OF_STRING;
+
+        char c = messedCalibrations[i++];
+
+        if ('\n' == c) return LINE_BREAK;
+        if ('0' <= c && c <= '9') return c - '0';
+
+        for (int j = 0; j < 9; j++) {
+            string num = spelledNumbers[j];
+            short len = num.size();
+
+            if (messedCalibrations.substr(i - 1, len) == num) return j + 1;
+        }
+
+        return NOT_A_NUMBER;
+    }
+};
+
+int Solution_01::sumCalibrations(string input)
+{
+    CalibrationsWalker *walker = new CalibrationsWalker(input);
+
+    int ans = 0;
+    short lastDigit = CalibrationsWalker::NOT_A_NUMBER;
+
+    short iterator = walker->getNext();
+    while (CalibrationsWalker::END_OF_STRING != iterator) {
+        if (iterator >= 0) {
+            if (lastDigit == CalibrationsWalker::NOT_A_NUMBER) ans += 10 * iterator;
+            lastDigit = iterator;
+        }
+        else if (iterator == CalibrationsWalker::LINE_BREAK) {
+            ans += lastDigit;
+            lastDigit = CalibrationsWalker::NOT_A_NUMBER;
+        }
+
+        iterator = walker->getNext();
+    }
+
+    return ans + lastDigit;
 }
 
