0062335826f8479ad089b55271e75c9af414ab68
Felipe Martins <6557756+fefas@users.noreply.github.com>
2023-12-04 12:09:54 +0100

    Solve two matching numbers on one sorted card

--- src/04-scratchcards.cpp
+++ src/04-scratchcards.cpp
@@ -6,9 +6,12 @@ using namespace std;
 
 int Solution_04::part1(vector<int> winningNumbers, vector<int> gottenNumbers)
 {
-    for (int i = 0; i < gottenNumbers.size(); i++)
-        for (int j = 0; j < winningNumbers.size(); j++)
-            if (gottenNumbers[i] == winningNumbers[j]) return 1;
+    int ans = 0;
 
-    return 0;
+    for (int i = 0; i < gottenNumbers.size(); i++) {
+        for (int j = 0; j < winningNumbers.size(); j++)
+            if (gottenNumbers[i] == winningNumbers[j]) ans++;
+    }
+
+    return ans;
 }
--- tests/04-scratchcards.cpp
+++ tests/04-scratchcards.cpp
@@ -8,11 +8,15 @@ using namespace std;
 namespace {
     Solution_04 *solution = new Solution_04();
 
-    TEST(Scratchcards, oneCardsAndNoMatching) {
+    TEST(Scratchcards, oneCardAndNoMatchingNumber) {
         ASSERT_EQ(0, solution->part1({ 2, 4, 5 }, { 3 }));
     }
 
-    TEST(Scratchcards, oneCardsAndOneMatching) {
+    TEST(Scratchcards, oneCardAndOneMatchingNumber) {
         ASSERT_EQ(1, solution->part1({ 2, 3, 5 }, { 3 }));
+    }
+
+    TEST(Scratchcards, oneSortedCardAndTwoMatchingNumbers) {
+        ASSERT_EQ(2, solution->part1({ 2, 3, 5 }, { 3, 5 }));
     }
 }
