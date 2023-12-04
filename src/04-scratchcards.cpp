f894293d9d578eaac56ba3ccfa61e234f51b8eb2
Felipe Martins <6557756+fefas@users.noreply.github.com>
2023-12-04 12:51:33 +0100

    Extract Card class

--- src/04-scratchcards.cpp
+++ src/04-scratchcards.cpp
@@ -6,29 +6,52 @@ using namespace std;
 
 #include "04-scratchcards.h"
 
-int Solution_04::part1(vector<vector<vector<int>>> cards)
+class Card
 {
-    vector<int> winningNumbers, gottenNumbers;
-    int ans = 0;
+    private:
+    vector<int> winningNumbers;
+    vector<int> gottenNumbers;
 
-    for (auto c : cards) {
-        winningNumbers = c[0];
-        gottenNumbers = c[1];
-
+    public:
+    Card(vector<vector<int>> input) : winningNumbers(input[0]), gottenNumbers(input[1])
+    {
         sort(winningNumbers.begin(), winningNumbers.end());
         sort(gottenNumbers.begin(), gottenNumbers.end());
+    }
 
+    int countMatches()
+    {
         int i = 0, j = 0, count = 0;
+
         while (i < gottenNumbers.size() && j < winningNumbers.size()) {
-            if (gottenNumbers[i] > winningNumbers[j]) j++;
-            else if (gottenNumbers[i] < winningNumbers[j]) i++;
-            else {
-                count++;
+            if (gottenNumbers[i] > winningNumbers[j]) {
                 j++;
-                i++;
+                continue;
             }
+            else if (gottenNumbers[i] < winningNumbers[j]) {
+                i++;
+                continue;
+            }
+
+            count++;
+            j++;
+            i++;
         }
 
+        return count;
+    }
+};
+
+int Solution_04::part1(vector<vector<vector<int>>> input)
+{
+    vector<Card*> cards;
+    int ans = 0;
+
+    for (auto i : input)
+        cards.push_back(new Card(i));
+
+    for (auto c : cards) {
+        int count = c->countMatches();
         ans += count ? pow(2, count - 1) : 0;
     }
 
