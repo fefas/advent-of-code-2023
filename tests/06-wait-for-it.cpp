c47eb27234503dc25420492db2bc07349fd59972
Felipe Martins <6557756+fefas@users.noreply.github.com>
2023-12-10 03:11:12 +0100

    Add test solving my puzzle part1 and part2

--- tests/06-wait-for-it.cpp
+++ tests/06-wait-for-it.cpp
@@ -15,11 +15,28 @@ namespace {
         ASSERT_EQ(9, solution->part1({ { 30, 200 } }));
     }
 
+    TEST(WaitForIt, oneRaceWithEvenTimasdasd) {
+        ASSERT_EQ(71503, solution->part1({ { 71530, 940200 } }));
+    }
+
     TEST(WaitForIt, sampleInput) {
         ASSERT_EQ(288, solution->part1({
             { 7, 9 },
             { 15, 40 },
             { 30, 200 },
+        }));
+    }
+
+    TEST(WaitForIt, myPuzzleInput) {
+        ASSERT_EQ(6209190, solution->part1({
+            { 40, 215 },
+            { 92, 1064 },
+            { 97, 1505 },
+            { 90, 1100 },
+        }));
+
+        ASSERT_EQ(28545089, solution->part1({
+            { 40929790, 215106415051100 },
         }));
     }
 }
