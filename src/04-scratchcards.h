90f4096b8f01d6b163c91051cac2e0cae8cebad7
Felipe Martins <6557756+fefas@users.noreply.github.com>
2023-12-11 01:04:44 +0100

    Day 04 - Scratchcards (#6)

---
+++ src/04-scratchcards.h
@@ -0,0 +1,10 @@
+#include <vector>
+
+using namespace std;
+
+class Solution_04
+{
+    public:
+    int part1(vector<vector<vector<int>>> cards);
+    int part2(vector<vector<vector<int>>> cards);
+};
