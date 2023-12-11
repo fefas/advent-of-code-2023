15c54637f2a5124dbfec07732c84fb4254e23e7b
Felipe Martins <6557756+fefas@users.noreply.github.com>
2023-12-11 01:00:04 +0100

    Day 02 - Cube Canundrum (#4)

---
+++ src/02-cube-canundrum.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include "02-cube-canundrum.h"
+
+using namespace std;
+
+int Solution_02::part1(NumberOfCubes *config, vector<Game*> games)
+{
+    int ans = 0;
+
+    for (int i = 0; i < games.size(); i++) {
+        if (games[i]->comply(config)) ans += i + 1;
+    }
+
+    return ans;
+};
+
+int Solution_02::part2(vector<Game*> games)
+{
+    int ans = 0;
+
+    for (auto g : games) {
+        ans += g->powerOfNeededCubesSet();
+    }
+
+    return ans;
+};
