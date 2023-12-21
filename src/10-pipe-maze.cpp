db3de3d2d7f242aac24c2943e35fc8adfad4c787
Felipe Martins <6557756+fefas@users.noreply.github.com>
2023-12-21 02:49:24 +0100

    Solves when starting at 1,1

--- src/10-pipe-maze.cpp
+++ src/10-pipe-maze.cpp
@@ -1,13 +1,74 @@
+#include <iostream>
 #include <string>
 #include <vector>
-
-#include <iostream>
 
 #include "10-pipe-maze.h"
 
 using namespace std;
 
+class PipePath
+{
+    private:
+    const vector<string> m;
+    int x, y;
+    int stepCount;
+    char prevPipe;
+
+    public:
+    PipePath(vector<string> _m, int _x, int _y) : m(_m), x(_x), y(_y)
+    {
+        stepCount = 1;
+        prevPipe = 'S';
+    }
+
+    bool operator!=(const PipePath& that)
+    {
+        return x != that.x || y != that.y;
+    }
+
+    int length()
+    {
+        return stepCount;
+    }
+
+    void walk()
+    {
+        char currPipe = m[x][y];
+
+        if (isNext(x - 1, y)) x--;
+        else if (isNext(x + 1, y)) x++;
+        else if (isNext(x, y - 1)) y--;
+        else if (isNext(x, y + 1)) y++;
+
+        stepCount++;
+        prevPipe = currPipe;
+    }
+//
+//    void print()
+//    {
+//        cout << x << ',' << y << ": " << m[x][y] << " >>> " << stepCount << endl;
+//    }
+
+    private:
+    bool isNext(int newX, int newY)
+    {
+        return m[newX][newY] != '.' && m[newX][newY] != prevPipe;
+    }
+};
+
 int Solution_10::part1(vector<string> input)
 {
-    return 2;
+    PipePath a(input, 2, 1);
+    PipePath b(input, 1, 2);
+
+//    a.print();
+//    b.print();
+    do {
+        a.walk();
+        b.walk();
+//        a.print();
+//        b.print();
+    } while (a != b);
+
+    return a.length();
 };
--- tests/10-pipe-maze.cpp
+++ tests/10-pipe-maze.cpp
@@ -12,9 +12,19 @@ namespace {
     TEST(PipeMaze, firstSample) {
         ASSERT_EQ(2, solution->part1({
             "....",
-            ".F7.",
+            ".S7.",
             ".LJ.",
             "....",
+        }));
+    }
+
+    TEST(PipeMaze, secondSample) {
+        ASSERT_EQ(4, solution->part1({
+            ".....",
+            ".S-7.",
+            ".|.|.",
+            ".L-J.",
+            ".....",
         }));
     }
 }
